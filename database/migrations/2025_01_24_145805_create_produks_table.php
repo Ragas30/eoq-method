ba94a74a81f1315ec513c2833a3298ccad653b77
Roman Keanu Mufid <romankeanumufid@gmail.com>
2025-01-24 22:03:10 +0700

    feat: add Produk migration and model

---
+++ database/migrations/2025_01_24_145805_create_produks_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produk', function (Blueprint $table) {
+            $table->string('kd_produk', 10);
+            $table->text('nm_produk');
+            $table->integer('stok');
+            $table->text('satuan');
+            $table->integer('harga');
+            $table->integer('harga_beli');
+            $table->text('deskripsi');
+            $table->text('gambar');
+            $table->double('lead_time');
+            $table->double('b_pesan');
+            $table->double('b_simpan');
+            $table->double('stok_cadangan');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produks');
+    }
+};
