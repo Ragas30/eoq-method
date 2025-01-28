971100db7c352ae6613b5bcdfefd1690e0be2683
Roman Keanu Mufid <romankeanumufid@gmail.com>
2025-01-28 14:16:53 +0700

    feat: Add Ongkir Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,6 +6,7 @@ use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Database\Seeders\UserSeeder;
+use Database\Seeders\OngkirSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -17,7 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            UserSeeder::class
+            UserSeeder::class,
+            OngkirSeeder::class
         ]);
     }
 }
