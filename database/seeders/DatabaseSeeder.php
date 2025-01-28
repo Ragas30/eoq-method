c0e4bc0a2ad86f27c1f33e82ea8a04b4ee38efbb
Roman Keanu Mufid <romankeanumufid@gmail.com>
2025-01-28 15:15:54 +0700

    refactor: Implementing Supplier seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         $this->call([
             UserSeeder::class,
-            OngkirSeeder::class
+            OngkirSeeder::class,
+            SupplierSeeder::class
         ]);
     }
 }
