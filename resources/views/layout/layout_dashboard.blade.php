bbfa8524069ca1f7fb537f8ea03aea7f038e401d
gmn26 <ghazynovrial@gmail.com>
2025-01-28 22:00:39 +0700

    refactor : fix vertical overflow for smaller viewport

--- resources/views/layout/layout_dashboard.blade.php
+++ resources/views/layout/layout_dashboard.blade.php
@@ -2,7 +2,7 @@
 
 <body class="font-sans antialiased flex h-screen dark:bg-black dark:text-white/50">
     <x-dashboard.sidebar></x-dashboard.sidebar>
-    <main class="bg-white text-black grow p-8">
+    <main class="bg-white text-black grow p-8 max-h-screen overflow-y-scroll">
         @yield('content')
     </main>
 </body>
