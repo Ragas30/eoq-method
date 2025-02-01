2673f3127bae97806b7772c7c01b0325e1b4251e
gmn26 <ghazynovrial@gmail.com>
2025-02-01 22:04:29 +0700

    refactor : redesign product's detail UI

--- resources/views/components/users/header.blade.php
+++ resources/views/components/users/header.blade.php
@@ -7,6 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
 </head>
 
 <body>
