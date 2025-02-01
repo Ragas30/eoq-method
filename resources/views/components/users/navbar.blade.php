59f5e5338712bcaae03d6cc09dd7dcbf6498369c
Ragas <ragasalgaluh@gmail.com>
2025-02-02 03:49:24 +0700

    renew user navbar

--- resources/views/components/users/navbar.blade.php
+++ resources/views/components/users/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="bg-white shadow-lg border-b border-gray-300 fixed top-0 left-0 right-0 z-50 sm:px-6 lg:px-8">
     <div class="container mx-auto px-4 py-2 flex justify-between items-center">
-        <a href="#" class="text-2xl font-bold text-gray-800">Logo</a>
+        <a href="{{ route('home') }}" class="text-2xl font-bold text-gray-800">Logo</a>
         <button class="bg-transparent border-none focus:outline-none sm:hidden" id="toggle-menu">
             <svg class="w-6 h-6 fill-current" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"
@@ -29,19 +29,35 @@
                     id="dropdown-more">
                     <ul class="py-1 text-sm text-gray-700 dark:text-gray-400" aria-labelledby="dropdown-more-button">
                         <li>
-                            <a href="#"
-                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-white">Setting</a>
+                            <a href="{{ route('cart') }}"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-white">Keranjang</a>
                         </li>
                         <li>
-                            <a href="#"
-                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-white">Logout</a>
+                            <a href="{{ route('order') }}"
+                                class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-white">Status
+                                Pesanan</a>
                         </li>
                     </ul>
                 </div>
             </li>
-            <li><a href="{{ route('login') }}"
-                    class="block py-2 px-3 text-gray-700 hover:bg-gray-100 sm:hover:bg-transparent sm:border-0 sm:hover:text-blue-700 sm:p-0 dark:text-gray-400 sm:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 sm:dark:hover:bg-transparent rounded-lg">Login</a>
-            </li>
+            @auth
+                <li>
+                    <form action="{{ route('logout') }}" method="POST" class="inline">
+                        @csrf
+                        <button type="submit"
+                            class="block py-2 px-3 text-gray-700 hover:bg-gray-100 sm:hover:bg-transparent sm:border-0 sm:hover:text-blue-700 sm:p-0 dark:text-gray-400 sm:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 sm:dark:hover:bg-transparent rounded-lg">
+                            Logout
+                        </button>
+                    </form>
+                </li>
+            @else
+                <li>
+                    <a href="{{ route('login') }}"
+                        class="block py-2 px-3 text-gray-700 hover:bg-gray-100 sm:hover:bg-transparent sm:border-0 sm:hover:text-blue-700 sm:p-0 dark:text-gray-400 sm:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 sm:dark:hover:bg-transparent rounded-lg">
+                        Login
+                    </a>
+                </li>
+            @endauth
         </ul>
     </div>
 </nav>
